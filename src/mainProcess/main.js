b2911ae996801dd16eaa32f62622461dc17073f9
ggooltae <waterboy94@naver.com>
2022-11-14 11:32:43 +0900

    chore: mainWindow 최소크기 지정

--- src/mainProcess/main.js
+++ src/mainProcess/main.js
@@ -5,6 +5,8 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 800,
+    minHeight: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,
