6aa1c1419458fdf621d4a636fc18be3c23accd2f
ggooltae <waterboy94@naver.com>
2022-11-14 21:50:40 +0900

    feat: NotFound 컴포넌트 내 로그인화면 이동버튼 생성

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <div>NotFound</div>
+      <button onClick={() => navigate("/", { replace: true })}>
+        로그인 화면으로 이동
+      </button>
+    </>
+  );
 }
