573dd0b515a10da7fbe5374fd7934c6e4f0a6c79
ggooltae <waterboy94@naver.com>
2022-11-14 11:31:55 +0900

    chore: 라우터설정

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,31 @@
-function App() {
-  return <div>hi</div>;
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+
+import Login from "../components/Login";
+import Home from "../components/Home";
+
+export default function App() {
+  return (
+    <>
+      <Global />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App;
+const Global = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    user-select: none;
+  }
+
+  button{
+    cursor: pointer;
+    border: none;
+  }
+`;
