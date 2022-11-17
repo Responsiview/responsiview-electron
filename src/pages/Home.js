e583a964966fd55f34134e7ff7a72b276f6fd075
ggooltae <waterboy94@naver.com>
2022-11-17 13:00:49 +0900

    feat: 메인페이지 mount시 cookie 유저정보 redux에 업데이트(새로고침 대응)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
+import Cookie from "js-cookie";
 
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import DeviceSection from "../components/DeviceSection";
 
+import { setUserInfo } from "../features/user/userSlice";
+
 import { COLOR } from "../config/constants";
 
 export default function Home() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setUserInfo({ userEmail: Cookie.get("userEmail") }));
+  }, []);
+
   return (
     <>
       <Header />
