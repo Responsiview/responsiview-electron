4b21b753bd560c5f97c86790f662190e54c1254d
ggooltae <waterboy94@naver.com>
2022-11-19 16:05:13 +0900

    fix: 스크롤비율 동기화중 최상단,최하단으로 이동 안되던 버그 수정

--- src/mainProcess/webview_preload.js
+++ src/mainProcess/webview_preload.js
@@ -6,6 +6,7 @@ function handleScrollEvent(event) {
   let scrollTop = Math.round(window.scrollY);
   let scrollHeight = Math.round(document.body.scrollHeight);
   let clientHeight = Math.round(window.innerHeight);
+
   let contentHeight = scrollHeight - clientHeight;
   let ratio = Math.round((scrollTop / contentHeight) * 100);
 
@@ -15,12 +16,19 @@ function handleScrollEvent(event) {
 window.addEventListener("scroll", handleScrollEvent);
 
 channel.onmessage = (event) => {
-  if (Math.abs(prevRatio - event.data) <= 1) return;
-
-  prevRatio = event.data;
-
   let scrollHeight = Math.round(document.body.scrollHeight);
   let clientHeight = Math.round(window.innerHeight);
 
+  if (event.data === 0) {
+    window.scrollTo(0, 0);
+    return;
+  }
+  if (event.data === 100) {
+    window.scrollTo(0, scrollHeight - clientHeight);
+    return;
+  }
+  if (Math.abs(prevRatio - event.data) <= 1) return;
+
+  prevRatio = event.data;
   window.scrollTo(0, (event.data * (scrollHeight - clientHeight)) / 100);
 };
