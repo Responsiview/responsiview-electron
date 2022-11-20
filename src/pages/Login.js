c2ea4fa102ffbe7fbaaf3fd14821a231f8a5c426
ggooltae <waterboy94@naver.com>
2022-11-20 15:34:09 +0900

    design: 로그인화면 디자인 수정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
+import { FcGoogle } from "react-icons/fc";
 
 import { setUserInfo } from "../features/user/userSlice";
 
@@ -37,8 +38,10 @@ export default function Login() {
           }
         }}
       >
-        <Icon className="fa-brands fa-google" />
-        Sign in with Google
+        <IconContainer>
+          <FcGoogle />
+        </IconContainer>
+        <TextContainer>Sign in with Google</TextContainer>
       </SignInButton>
     </Content>
   );
@@ -51,31 +54,44 @@ const Content = styled.div`
   align-items: center;
   width: 100vw;
   height: 100vh;
-  background-color: ${COLOR.DARK_NAVY};
+  background-color: ${COLOR.LIGHT_GRAY};
 `;
 
-const Title = styled.p`
-  font-size: 3rem;
-  color: ${COLOR.IVORY};
+const Title = styled.span`
+  font-size: 4rem;
+  font-weight: bold;
+  color: ${COLOR.DARK_NAVY};
 `;
 
 const SignInButton = styled.button`
   display: flex;
+  justify-content: space-between;
   align-items: center;
-  margin-top: 3rem;
+  margin-top: 5rem;
+  height: 3rem;
+  width: 16rem;
   padding: 0.5rem 2rem;
   border-radius: 5px;
   font-size: 1rem;
-  background-color: ${COLOR.DARK_BLUE};
+  background-color: ${COLOR.DARK_NAVY};
   color: ${COLOR.IVORY};
 
   &:hover {
     background-color: ${COLOR.IVORY};
-    color: ${COLOR.DARK_BLUE};
+    color: ${COLOR.DARK_NAVY};
   }
 `;
 
-const Icon = styled.i`
-  margin-right: 0.5rem;
-  font-size: 1.1rem;
+const IconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.7rem;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
 `;
