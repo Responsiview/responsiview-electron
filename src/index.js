73b780d4d626305ce218c0005bd040b6eddad004
ggooltae <waterboy94@naver.com>
2022-11-25 12:04:22 +0900

    refactor: file-base 라우팅을 위해 hashrouter 적용

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import store from "./app/configureStore";
@@ -10,9 +10,9 @@ import App from "./app/App";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
